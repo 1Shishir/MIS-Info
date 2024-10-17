5268aa0aed4236d1b114e7e139af5da0feabfdbe
1Shishir <hasanur.rahman.shishir.iit@gmail.com>
2024-10-17 12:56:20 +0600

    firebase added successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mis_info/firebase_options.dart';
 import 'package:mis_info/screen/home/home_screen.dart';
 
-void main(){
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
